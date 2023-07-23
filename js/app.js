32102d9288692fe913075a375f448c99dd02d5c1
lezcore <bianorsergior@gmail.com>
2023-07-22 21:36:27 -0500

    scripts

--- js/app.js
+++ js/app.js
@@ -1,14 +1,16 @@
 function login (){
   var user, pass;
-  user = document.getElementById("first-name");
-  pass = document.getElementById("new-password");
+  user = document.getElementById("first-name").value;
+  pass = document.getElementById("new-password").value;
   
-  if (user == "user" && pass == "pass") {
+  if (user === "user" && pass === "12345") {
+    alert("bienvenido");
     window.location = "videos.html";
+    // location.href = "videos.html";
 
-  }  else {
-    alert("Por favor ingrese los datos correctos")
-    window.location = "error.html"
- }
+  }else {
+    alert("Por favor ingrese los datos correctos");
+    window.location = "error.html";
+  }
 }
 
