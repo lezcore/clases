57318eb374c9d5f03bf453dd1cfa9a34f593c566
lezcore <bianorsergior@gmail.com>
2023-07-23 18:57:22 -0500

    functions in data.js

--- datos.js
+++ datos.js
@@ -1,67 +1,60 @@
 // var introTitle = "Titulo de la seccion";
 // var introText = "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quam at delectus animi fugit ut dolorum similique sit iste doloremque illo deserunt, autem eius tempore, incidunt ratione accusamus molestiae laborum suscipit?";
 
-function mostrar () {
-  document.getElementById('valueOne').textContent = modOneTitle;
+function showOne() {
+  document.getElementById('two').style.display = 'none';
+  document.getElementById('three').style.display = 'none';
+  document.getElementById('four').style.display = 'none';
+  document.getElementById('one').style.display = 'block';
 }
-function mostrando () {
-  document.getElementById('valueDefault').textContent = modOneText;
+function showTwo() {
+  document.getElementById('one').style.display = 'none';
+  document.getElementById('three').style.display = 'none';
+  document.getElementById('four').style.display = 'none';
+  document.getElementById('two').style.display = 'block';
+}
+function showThree() {
+  document.getElementById('one').style.display = 'none';
+  document.getElementById('two').style.display = 'none';
+  document.getElementById('four').style.display = 'none';
+  document.getElementById('three').style.display = 'block';
+}
+function showFour() {
+  document.getElementById('one').style.display = 'none';
+  document.getElementById('two').style.display = 'none';
+  document.getElementById('three').style.display = 'none';
+  document.getElementById('four').style.display = 'block';
 }
 
-var modOneTitle = "Seccion 1";
-var modOneText = "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quam at delectus animi fugit ut dolorum similique sit iste doloremque illo deserunt, autem eius tempore, incidunt ratione accusamus molestiae laborum suscipit?";
 
-var modTwoTitle = "Titulo de la seccion 2";
-var modTwoText = "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quam at delectus animi fugit ut dolorum similique sit iste doloremque illo deserunt, autem eius tempore, incidunt ratione accusamus molestiae laborum suscipit?";
 
-var modThreeTitle = "Titulo de la seccion 3";
-var modThreeText = "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quam at delectus animi fugit ut dolorum similique sit iste doloremque illo deserunt, autem eius tempore, incidunt ratione accusamus molestiae laborum suscipit?";
 
-var modFourTitle = "Titulo de la seccion 4";
-var modFourText = "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quam at delectus animi fugit ut dolorum similique sit iste doloremque illo deserunt, autem eius tempore, incidunt ratione accusamus molestiae laborum suscipit?";
 
-var title = document.getElementById('introTitle').textContent;
+// function behind() {
+//   document.getElementById('idName').style.display = 'none';
+// }
+
+
+// function mostrar () {
+//   document.getElementById('valueOne').textContent = modOneTitle;
+// }
+// function mostrando () {
+//   document.getElementById('valueDefault').textContent = modOneText;
+// }
+
+
+// var modOneTitle = "Seccion 1";
+// var modOneText = "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quam at delectus animi fugit ut dolorum similique sit iste doloremque illo deserunt, autem eius tempore, incidunt ratione accusamus molestiae laborum suscipit?";
+
+// var modTwoTitle = "Titulo de la seccion 2";
+// var modTwoText = "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quam at delectus animi fugit ut dolorum similique sit iste doloremque illo deserunt, autem eius tempore, incidunt ratione accusamus molestiae laborum suscipit?";
+
+// var modThreeTitle = "Titulo de la seccion 3";
+// var modThreeText = "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quam at delectus animi fugit ut dolorum similique sit iste doloremque illo deserunt, autem eius tempore, incidunt ratione accusamus molestiae laborum suscipit?";
+
+// var modFourTitle = "Titulo de la seccion 4";
+// var modFourText = "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quam at delectus animi fugit ut dolorum similique sit iste doloremque illo deserunt, autem eius tempore, incidunt ratione accusamus molestiae laborum suscipit?";
+
+// var title = document.getElementById('introTitle').textContent;
 
 // alert("hola estoy funcionando");
-
-// function change (titulo, description, newTitle, newText){
-//   document.getElementById(titulo).value = newTitle.value;
-//   document.getElementById(description).value = newText.value;
-  
-
-
-//   document.getElementById('introTitle').display = "none";
-//   document.getElementById('introDescription').display = "none";
-//   document.getElementById('introTitle').display = "block";
-//   document.getElementById('introDescription').display = "block";
-// }
-
-// function valor1(change) {
-//   change(title, text, modOneTitle, modOneText);
-// }
-// function valor1(change) {
-//   change(modTwoTitle, modTwoText);
-// }
-
-// function introduccion(change) {
-//   change();
-// }
-
-
-
-// 
-// 
-// 
-
-
-// function nivel (title, newTitle, text, newText, img, newImg, btn, newBtn, color, newColor ) {
-//   document.getElementById(title).textContent=newTitle;
-//   document.getElementById(text).textContent=newText;
-//   img.src=newImg;
-//   btn.style.backgroundColor=color;
-//   newBtn.style.backgroundColor=newColor;
-// }
-
-
-// function Primaria(nivel) { nivel("edu-ini", title_prim, "text-ini", text_prim, img_ini,
-//  "tagore/grupito.jpeg", btn_ini, btn_prim, color2, color1) };
