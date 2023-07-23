3b933c2cd261c5b70bba1fa2d638f4997a97ca70
lezcore <bianorsergior@gmail.com>
2023-07-22 22:34:49 -0500

    mod datos.js

--- datos.js
+++ datos.js
@@ -1,6 +1,13 @@
 // var introTitle = "Titulo de la seccion";
 // var introText = "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quam at delectus animi fugit ut dolorum similique sit iste doloremque illo deserunt, autem eius tempore, incidunt ratione accusamus molestiae laborum suscipit?";
 
+function mostrar () {
+  document.getElementById('valueOne').textContent = modOneTitle;
+}
+function mostrando () {
+  document.getElementById('valueDefault').textContent = modOneText;
+}
+
 var modOneTitle = "Seccion 1";
 var modOneText = "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quam at delectus animi fugit ut dolorum similique sit iste doloremque illo deserunt, autem eius tempore, incidunt ratione accusamus molestiae laborum suscipit?";
 
@@ -14,26 +21,24 @@ var modFourTitle = "Titulo de la seccion 4";
 var modFourText = "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quam at delectus animi fugit ut dolorum similique sit iste doloremque illo deserunt, autem eius tempore, incidunt ratione accusamus molestiae laborum suscipit?";
 
 var title = document.getElementById('introTitle').textContent;
-var text = document.getElementById('introDescription').textContent;
 
-function prueba () {
-  alert("hola estoy funcionando");
-}
-function change (titulo, description, newTitle, newText){
-  document.getElementById(titulo).value = newTitle.value;
-  document.getElementById(description).value = newText.value;
+// alert("hola estoy funcionando");
+
+// function change (titulo, description, newTitle, newText){
+//   document.getElementById(titulo).value = newTitle.value;
+//   document.getElementById(description).value = newText.value;
   
 
 
-  // document.getElementById('introTitle').display = "none";
-  // document.getElementById('introDescription').display = "none";
-  // document.getElementById('introTitle').display = "block";
-  // document.getElementById('introDescription').display = "block";
-}
+//   document.getElementById('introTitle').display = "none";
+//   document.getElementById('introDescription').display = "none";
+//   document.getElementById('introTitle').display = "block";
+//   document.getElementById('introDescription').display = "block";
+// }
 
-function valor1(change) {
-  change(title, text, modOneTitle, modOneText);
-}
+// function valor1(change) {
+//   change(title, text, modOneTitle, modOneText);
+// }
 // function valor1(change) {
 //   change(modTwoTitle, modTwoText);
 // }
@@ -47,14 +52,16 @@ function valor1(change) {
 // 
 // 
 // 
-function nivel (title, newTitle, text, newText, img, newImg, btn, newBtn, color, newColor ) {
-  document.getElementById(title).textContent=newTitle;
-  document.getElementById(text).textContent=newText;
-  img.src=newImg;
-  btn.style.backgroundColor=color;
-  newBtn.style.backgroundColor=newColor;
-}
 
 
-function Primaria(nivel) { nivel("edu-ini", title_prim, "text-ini", text_prim, img_ini,
- "tagore/grupito.jpeg", btn_ini, btn_prim, color2, color1) };
+// function nivel (title, newTitle, text, newText, img, newImg, btn, newBtn, color, newColor ) {
+//   document.getElementById(title).textContent=newTitle;
+//   document.getElementById(text).textContent=newText;
+//   img.src=newImg;
+//   btn.style.backgroundColor=color;
+//   newBtn.style.backgroundColor=newColor;
+// }
+
+
+// function Primaria(nivel) { nivel("edu-ini", title_prim, "text-ini", text_prim, img_ini,
+//  "tagore/grupito.jpeg", btn_ini, btn_prim, color2, color1) };
